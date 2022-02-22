b3afb53b0a99179d2e3b03767234c9d6b8de69b4
FLSoz <65517812+FLSoz@users.noreply.github.com>
2022-02-21 18:28:13 -0800

    Initial code

--- FuelRegenPatch.cs
+++ FuelRegenPatch.cs
@@ -7,22 +7,52 @@ namespace FuelRegen
     public class FuelRegenPatch : ModBase
     {
         internal static Harmony harmony;
+        internal const float scale = 2.0f;
 
         public override void DeInit()
         {
             harmony.UnpatchAll("com.flsoz.ttmods.fuelregenpatch");
+
+            Singleton.camera.farClipPlane /= scale;
+
+            CameraManager manCamera = Singleton.Manager<CameraManager>.inst;
+            manCamera.SetDetailDist01(manCamera.DetailDist01 / scale);
+            manCamera.SetDrawDist01(manCamera.DrawDist01 / scale);
         }
 
         public override void Init()
         {
             harmony = new Harmony("com.flsoz.ttmods.fuelregenpatch");
             harmony.PatchAll();
+
+            Singleton.camera.farClipPlane *= scale;
+
+            CameraManager manCamera = Singleton.Manager<CameraManager>.inst;
+            manCamera.SetDetailDist01(manCamera.DetailDist01);
+            manCamera.SetDrawDist01(manCamera.DrawDist01);
+        }
+
+        [HarmonyPatch(typeof(CameraManager), "SetDrawDist01")]
+        public static class PatchDrawDist
+        {
+            public static void Prefix(ref float drawDist01)
+            {
+                drawDist01 = drawDist01 * scale;
+            }
+        }
+        [HarmonyPatch(typeof(CameraManager), "SetDetailDist01")]
+        public static class PatchDetailDist
+        {
+            public static void Prefix(ref float detailDist01)
+            {
+                detailDist01 = detailDist01 * scale;
+            }
         }
 
         [HarmonyPatch(typeof(TechBooster), "FixedUpdate")]
         public static class Patch
         {
-            private static MethodInfo get_Boosters = typeof(TechBooster).GetProperty("BoostersFiring", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetGetMethod();
+            private static readonly MethodInfo get_Boosters = typeof(TechBooster).GetProperty("BoostersFiring", BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic).GetGetMethod();
 
             public static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
             {
